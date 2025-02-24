f70882ccf494463448fa0d59de18607d4a909456
Wei Dong <wdong@aaalgo.com>
2025-02-24 04:18:53 +0000

    add

---
+++ aa_tools/aa_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+from . import *
+
+def main():
+    range_ = "1"
+    if len(sys.argv) > 1:
+        range_ = sys.argv[1]
+
+    begin, end = parse_range(range_, 20)
+
+    with aa_context() as aa:
+        aa.view(begin, end)
+
+if __name__ == "__main__":
+    main()
---
+++ aa_tools/aa_ticket.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+import requests
+import argparse
+
+
+def main():
+    current_directory = os.getcwd()
+    parent_dir, cur_dir = os.path.split(current_directory)
+    print(parent_diri, cur_dir)
